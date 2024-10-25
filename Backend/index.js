bbd39e8330e135f3584ecd819cf02cd64338b06e
77Oshnik <tiwarioshnikdeep@gmail.com>
2024-10-26 00:46:50 +0530

    basic setup of backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,18 @@
-// index.js
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-  app.get('/api/heatmap', async (req, res) => {
-    try {
-      const data = await Heatmap.find(); // Fetch data from MongoDB
-      res.json(data);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/user', userRoutes); // Use userRoutes as middleware
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
